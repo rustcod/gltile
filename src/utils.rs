e9edf93a2f049f0d196a191d8d63a3cbf16642b6
David Obrite <david@ministrycentered.com>
2017-03-02 19:01:38 -0800

    chore(utils): clean up; add read_bytes

--- src/utils.rs
+++ src/utils.rs
@@ -18,20 +18,31 @@ pub fn ortho_projection(screen_size: data::Size) -> mvp::Matrix4 {
     [[o_w, 0.0, 0.0, 0.0], [0.0, o_h, 0.0, 0.0], [0.0, 0.0, 1.0, 0.0], [0.0, 0.0, 0.0, 1.0]]
 }
 
-pub fn read_file(filename: &str) -> io::Result<(String)> {
+pub fn read_file(path: &str) -> io::Result<(String)> {
     use std::io::Read;
     use std::fs::File;
 
-    let mut file = try!(File::open(filename));
-    let mut src = String::new();
-    try!(file.read_to_string(&mut src));
-    Ok(src)
+    let mut file = try!(File::open(path));
+    let mut string = String::new();
+    try!(file.read_to_string(&mut string));
+    Ok(string)
 }
 
-pub fn read_png_to_texture(file_bytes: &[u8]) -> glium::texture::RawImage2d<u8> {
+pub fn read_bytes(path: &str) -> io::Result<Vec<u8>> {
+    use std::io::Read;
+    use std::fs::File;
+
+    let mut file = try!(File::open(path));
+    let mut bytes = Vec::new();
+    try!(file.read_to_end(&mut bytes));
+    Ok(bytes)
+}
+
+
+pub fn read_png_to_texture(bytes: &[u8]) -> glium::texture::RawImage2d<u8> {
     use image;
 
-    let image = image::load(io::Cursor::new(file_bytes), image::PNG).unwrap().to_rgba();
+    let image = image::load(io::Cursor::new(bytes), image::PNG).unwrap().to_rgba();
     let image_dimensions = image.dimensions();
     glium::texture::RawImage2d::from_raw_rgba_reversed(image.into_raw(), image_dimensions)
 }
