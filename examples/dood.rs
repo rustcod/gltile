c730d7506a5715468765f9d89a048aaf108b6254
David Obrite <david@ministrycentered.com>
2017-03-04 11:46:00 -0800

    feat(renderer): move glium code into Renderer

--- examples/dood.rs
+++ examples/dood.rs
@@ -8,59 +8,22 @@ extern crate image;
 extern crate gltile;
 
 fn main() {
-    use glium::Surface;
+    let mut renderer = gltile::Renderer::new(1536, /* 96 */
+                                             1024, /* 64 */
+                                             16,
+                                             "assets/tileset.png");
 
-    let renderer = gltile::Renderer::new(1536 /* 96 */, 1024 /* 64 */, 16);
-
-    let texture = {
-        let png = gltile::read_bytes("assets/tileset.png").unwrap();
-        let tileset_texture = gltile::read_png_to_texture(&png[..]);
-        glium::texture::Texture2d::new(&renderer.display, tileset_texture).unwrap()
+    let tile = {
+        let mut tile = gltile::Tile::new();
+        tile.fg = *gltile::colors::YELLOW;
+        tile.pix = gltile::Pix::Dood;
+        tile
     };
 
-    let tileset = texture.sampled().magnify_filter(glium::uniforms::MagnifySamplerFilter::Nearest);
-
-    let cam_uniforms = {
-        let mat4_id = gltile::mat4_id();
-        let ortho_projection = gltile::ortho_projection(renderer.screen_size);
-
-        let cam = [[1.0, 0.0, 0.0, 0.0],
-                   [0.0, 1.0, 0.0, 0.0],
-                   [0.0, 0.0, 1.0, 0.0],
-                   [-768.0, -512.0, 0.0, 1.0]];
-
-        uniform! {
-            mvp: gltile::model_view_projection(mat4_id, cam, ortho_projection),
-            tileset: tileset,
-        }
-    };
-
-    let vb = {
-        let mut vb = gltile::VertexBuffer::new(renderer.size);
-
-        let tile = {
-            let mut tile = gltile::Tile::new();
-            tile.fg = *gltile::colors::YELLOW;
-            tile.pix = gltile::Pix::Dood;
-            tile
-        };
-
-        vb.set(gltile::WindowLoc { x: 5, y: 5 }, tile);
-
-        // TODO hide this as an implementation detail
-        glium::VertexBuffer::new(&renderer.display, &vb.data()).unwrap()
-    };
+    renderer.set(gltile::WindowLoc { x: 5, y: 5 }, tile);
 
     loop {
-        let mut target = renderer.display.draw();
-        target.clear_color(0.0, 0.0, 0.0, 1.0);
-        target.draw(&vb,
-                  &renderer.indices,
-                  &renderer.program,
-                  &cam_uniforms,
-                  &Default::default())
-            .unwrap();
-        target.finish().unwrap();
+        renderer.render();
 
         for ev in renderer.display.poll_events() {
             match ev {
--- src/renderer.rs
+++ src/renderer.rs
@@ -1,6 +1,9 @@
 use glium;
 
+use data;
 use Size;
+use console;
+use mvp;
 use read_file;
 use utils;
 
@@ -10,17 +13,21 @@ pub struct Renderer {
     pub display: glium::backend::glutin_backend::GlutinFacade,
     pub program: glium::Program,
     pub indices: glium::IndexBuffer<u16>,
+    pub texture: glium::texture::Texture2d,
+    pub vertex_buffer: console::VertexBuffer,
 }
 
 impl Renderer {
-    pub fn new(width: i32, height: i32, tile_size: i32) -> Self {
+    pub fn new(width: i32, height: i32, tile_size: i32, texture_path: &str) -> Self {
         let screen_size = Size::new(width, height);
         let size = Size::new((screen_size.width / tile_size),
                              (screen_size.height / tile_size));
 
         let display = display(screen_size);
         let program = program(&display);
-        let indices = indices(size, &display);
+        let indices = indices(&display, size);
+        let texture = texture(&display, texture_path);
+        let vertex_buffer = console::VertexBuffer::new(size);
 
         Renderer {
             size: size,
@@ -28,7 +35,48 @@ impl Renderer {
             display: display,
             program: program,
             indices: indices,
+            texture: texture,
+            vertex_buffer: vertex_buffer,
         }
+    }
+
+    pub fn render(&self) {
+        use glium::Surface;
+
+        let vb = glium::VertexBuffer::new(&self.display, &self.vertex_buffer.data()).unwrap();
+
+        let cam_uniforms = {
+            let mat4_id = utils::mat4_id();
+            let ortho_projection = utils::ortho_projection(self.screen_size);
+
+            let cam = [[1.0, 0.0, 0.0, 0.0],
+                       [0.0, 1.0, 0.0, 0.0],
+                       [0.0, 0.0, 1.0, 0.0],
+                       [-768.0, -512.0, 0.0, 1.0]];
+
+            let tileset = self.texture
+                .sampled()
+                .magnify_filter(glium::uniforms::MagnifySamplerFilter::Nearest);
+
+            uniform! {
+                mvp: mvp::model_view_projection(mat4_id, cam, ortho_projection),
+                tileset: tileset,
+            }
+        };
+
+        let mut target = self.display.draw();
+        target.clear_color(0.0, 0.0, 0.0, 1.0);
+        target.draw(&vb,
+                  &self.indices,
+                  &self.program,
+                  &cam_uniforms,
+                  &Default::default())
+            .unwrap();
+        target.finish().unwrap();
+    }
+
+    pub fn set(&mut self, window_loc: data::WindowLoc, tile: console::Tile) {
+        self.vertex_buffer.set(window_loc, tile);
     }
 }
 
@@ -46,8 +94,8 @@ fn program(display: &glium::backend::glutin_backend::GlutinFacade) -> glium::Pro
     glium::Program::from_source(display as &glium::backend::Facade, &vertex, &frag, None).unwrap()
 }
 
-fn indices(size: Size,
-           display: &glium::backend::glutin_backend::GlutinFacade)
+fn indices(display: &glium::backend::glutin_backend::GlutinFacade,
+           size: Size)
            -> glium::IndexBuffer<u16> {
     use glium::index::PrimitiveType;
 
@@ -56,4 +104,12 @@ fn indices(size: Size,
                             PrimitiveType::TrianglesList,
                             &indices)
         .unwrap()
+}
+
+fn texture(display: &glium::backend::glutin_backend::GlutinFacade,
+           path: &str)
+           -> glium::texture::Texture2d {
+    let png = utils::read_bytes(path).unwrap();
+    let texture = utils::read_png_to_texture(&png[..]);
+    glium::texture::Texture2d::new(display as &glium::backend::Facade, texture).unwrap()
 }
