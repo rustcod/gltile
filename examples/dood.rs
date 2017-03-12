f23411dd0451f054bdea4abe14bf7bc83c185b3b
David Obrite <david@ministrycentered.com>
2017-03-11 17:00:57 -0800

    chore(examples): use Looper

--- examples/dood.rs
+++ examples/dood.rs
@@ -1,5 +1,6 @@
 extern crate glium;
 extern crate gltile;
+extern crate looper;
 
 fn main() {
     let mut renderer = gltile::Renderer::new(1536, 1024, 16, "assets/tileset.png");
@@ -13,14 +14,16 @@ fn main() {
 
     renderer.set(gltile::ScreenPoint2D::new(5, 5), tile);
 
-    loop {
+    looper::Looper::new(60.0).run(|| {
         renderer.render();
 
         for ev in renderer.display.poll_events() {
             match ev {
-                glium::glutin::Event::Closed => return,
+                glium::glutin::Event::Closed => return looper::Action::Stop,
                 _ => (),
             }
         }
-    }
+
+        looper::Action::Continue
+    })
 }
