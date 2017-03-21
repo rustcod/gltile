a65a3199b4c43b45ea874bca24f2c3d19d1f3de5
David Obrite <david@ministrycentered.com>
2017-03-20 17:57:23 -0700

    chore(renderer): move display out of renderer

--- examples/dood.rs
+++ examples/dood.rs
@@ -2,8 +2,15 @@ extern crate glium;
 extern crate gltile;
 extern crate looper;
 
+use glium::DisplayBuild;
+
 fn main() {
-    let mut renderer = gltile::Renderer::new(1536, 1024, 16, "assets/tileset.png");
+    let display = glium::glutin::WindowBuilder::new()
+        .with_dimensions(1536, 1024)
+        .build_glium()
+        .unwrap();
+
+    let mut renderer = gltile::Renderer::new(&display, 16, "assets/tileset.png");
 
     let tile = {
         let mut tile = gltile::Tile::new();
@@ -16,14 +23,12 @@ fn main() {
 
     looper::Looper::new(60.0).run(|| {
         renderer.render();
-
-        for ev in renderer.display.poll_events() {
+        for ev in display.poll_events() {
             match ev {
                 glium::glutin::Event::Closed => return looper::Action::Stop,
                 _ => (),
             }
         }
-
         looper::Action::Continue
     })
 }
--- src/renderer.rs
+++ src/renderer.rs
@@ -5,26 +5,36 @@ use shaders;
 use units;
 use utils;
 
-pub struct Renderer {
+fn get_screen_size(display: &glium::backend::glutin_backend::GlutinFacade) -> units::Size2D {
+    let factor = display.get_window().unwrap().hidpi_factor();
+    let (width, height) = display.get_framebuffer_dimensions();
+    units::Size2D::new((width as f32 / factor) as i32,
+                       (height as f32 / factor) as i32)
+}
+
+pub struct Renderer<'a> {
     pub screen_size: units::Size2D,
     pub size: units::Size2D,
-    pub display: glium::backend::glutin_backend::GlutinFacade,
+    pub display: &'a glium::backend::glutin_backend::GlutinFacade,
     pub program: glium::Program,
     pub indices: glium::IndexBuffer<u16>,
     pub texture: glium::texture::Texture2d,
     pub vertex_buffer: console::VertexBuffer,
 }
 
-impl Renderer {
-    pub fn new(width: i32, height: i32, tile_size: i32, texture_path: &str) -> Self {
-        let screen_size = units::Size2D::new(width, height);
+impl<'a> Renderer<'a> {
+    pub fn new(
+        display: &'a glium::backend::glutin_backend::GlutinFacade,
+        tile_size: i32,
+        texture_path: &str
+    ) -> Self {
+        let screen_size = get_screen_size(display);
         let size = units::Size2D::new((screen_size.width / tile_size),
                                       (screen_size.height / tile_size));
 
-        let display = display(screen_size);
-        let program = program(&display);
-        let indices = indices(&display, size);
-        let texture = texture(&display, texture_path);
+        let program = program(display);
+        let indices = indices(display, size);
+        let texture = texture(display, texture_path);
         let vertex_buffer = console::VertexBuffer::new(size);
 
         Renderer {
@@ -41,7 +51,7 @@ impl Renderer {
     pub fn render(&self) {
         use glium::Surface;
 
-        let vb = glium::VertexBuffer::new(&self.display, &self.vertex_buffer.data()).unwrap();
+        let vb = glium::VertexBuffer::new(self.display, &self.vertex_buffer.data()).unwrap();
 
         let cam_uniforms = {
             let mat4_id = utils::mat4_id();
@@ -76,14 +86,6 @@ impl Renderer {
     pub fn set(&mut self, screen_point: units::ScreenPoint2D, tile: console::Tile) {
         self.vertex_buffer.set(screen_point, tile);
     }
-}
-
-fn display(screen_size: units::Size2D) -> glium::backend::glutin_backend::GlutinFacade {
-    use glium::DisplayBuild;
-    glium::glutin::WindowBuilder::new()
-        .with_dimensions(screen_size.width as u32, screen_size.height as u32)
-        .build_glium()
-        .unwrap()
 }
 
 fn program(display: &glium::backend::glutin_backend::GlutinFacade) -> glium::Program {
